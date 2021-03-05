52d0c8ee1818a655257c222f7a0d8f9cc5c52bd3
Fernando Aldair <ferpool_ald@hotmail.com>
2021-03-05 10:46:57 -0600

    fix neotecnology

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['neotecnology.gt'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['neotecnology.gq'])
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
